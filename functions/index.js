7135ab4a4c49d60a45e6a1fab544b180301964e3
cuongle30 <cuongle30@gmail.com>
2020-07-13 11:25:40 -0700

    add valiations

--- functions/index.js
+++ functions/index.js
@@ -64,7 +64,20 @@ app.post('/scream', (req, res) => {
       console.error(err)
     })
 })
-
+// Validate user input
+const isEmpty = (string) => {
+  if (string.trim() === '') {
+    return true
+  } else {
+    return false
+  }
+}
+// validate email address
+const isEmail = (email) => {
+  const regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (email.match(regEx)) return true
+  else return false
+}
 // Signup route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -74,7 +87,32 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle
   }
 
+  // Validate for sign up errors
+  let errors = {}
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty'
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid Email'
+  }
+
+  if (isEmpty(newUser.password)) {
+    errors.password = 'Must not be empty'
+  }
+  if (newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = 'Password must match'
+  }
+  if (isEmpty(newUser.handle)) {
+    errors.handle = 'Must not be empty'
+  }
+
+  // Pass an error if any of the validation error occus
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors)
+  }
+
   // Validate user data
+  let token, userId
   db.doc(`/users/${newUser.handle}`).get()
     .then((doc) => {
       if (doc.exists) {
@@ -85,12 +123,22 @@ app.post('/signup', (req, res) => {
       }
     })
     .then((data) => {
+      userId = data.user.uid
       return data.user.getIdToken()
     })
-    .then((token) => {
+    .then((idToken) => {
+      token = idToken
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      }
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+    })
+    .then(() => {
       return res.status(201).json({ token })
-    }
-    )
+    })
     .catch((err) => {
       console.error(err)
       if (err.code === 'auth/email-already-in-use') {
@@ -98,6 +146,32 @@ app.post('/signup', (req, res) => {
       } else {
         return res.status(500).json({ error: err.code })
       }
+    })
+})
+
+// Login route
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  let errors = {}
+  if (isEmpty(user.email)) errors.email = 'Must not be empty'
+  if (isEmpty(user.password)) errors.password = 'Must not be empty'
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken()
+    })
+    .then(token => {
+      return res.json({ token })
+    })
+    .catch(err => {
+      console.error(err)
+      if (err.code === 'auth/wrong-password') {
+        return res.status(401).json({ general: 'Email/Password does not match' })
+      } else return res.status(500).json({ error: err.code })
     })
 })
 
